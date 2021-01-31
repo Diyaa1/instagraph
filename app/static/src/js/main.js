11132a067e9d5f186f3e8b2c84cec8e5c7a74401
shamsawamleh <sawamleh@iwdt.com.jo>
2021-01-31 20:21:07 +0200

    ui config

--- app/static/src/js/main.js
+++ app/static/src/js/main.js
@@ -7,7 +7,7 @@ import FollowersBatch from './components/FollowersBatch'
 import io from 'socket.io-client';
 // Define global packages
 window.axios = require('axios');
-window.socket = io("http://followerspicker:80");
+window.socket = io("https://followerspicker.com/");
 
 
 
